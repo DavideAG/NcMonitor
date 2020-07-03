a60c10c13e1940f2b4ee670217b98d3f2ed311c2
DavideAG <davideantonino94@gmail.com>
2020-07-03 02:14:59 +0200

    beta 1 - working

--- app/src/main/java/com/example/ncmonitor/MainActivity.kt
+++ app/src/main/java/com/example/ncmonitor/MainActivity.kt
@@ -16,8 +16,10 @@ import okhttp3.Credentials.basic
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
+import kotlin.math.round
 
-
+const val GB = 1073741824
+const val Byte = 1024
 const val PERMISSION_REQUEST = 10
 
 /* your Nextcloud username */
@@ -71,14 +73,15 @@ class MainActivity : WearableActivity() {
         }
     }
 
-    private fun requestNcStatus() {
-
+    private fun requestNcStatus()
+    {
         val credentials = basic(USERNAME, PASSWORD)
 
         request.GET(credentials, URL, object: Callback {
-            override fun onResponse(call: Call?, response: Response) {
+            override fun onResponse(call: Call?, response: Response)
+            {
                 val responseData = response.body()?.string()
-                runOnUiThread{
+                runOnUiThread {
                     try {
                         val json = JSONObject(responseData!!)
                         Log.d("requestNcStatus","Request Successful!!")
@@ -88,18 +91,44 @@ class MainActivity : WearableActivity() {
 
                         val responseObject = json.getJSONObject("ocs")
                         val metaObject = responseObject.getJSONObject("meta")
-                        status_code_response_used_placeholder.text = metaObject.getInt("statuscode").toString()
-                        status_message_response_placeholder.text = metaObject.getString("message")
 
-                        val dataObject =  responseObject.getJSONObject("data")
-                        val nextcloudObject =  dataObject.getJSONObject("nextcloud")
-                        val systemObject =  nextcloudObject.getJSONObject("system")
-                        val cpuLoadArray = systemObject.getJSONArray("cpuload")
+                        val statusCode = metaObject.getInt("statuscode")
+
+                        if (statusCode == 200) {
+                            status_code_response_layout.visibility = View.GONE
+                            status_message_response_layout.visibility = View.GONE
+
+                            val dataObject =  responseObject.getJSONObject("data")
+                            val nextcloudObject =  dataObject.getJSONObject("nextcloud")
+                            val systemObject =  nextcloudObject.getJSONObject("system")
+
+                            // cpu load
+                            val cpuLoad = systemObject.getJSONArray("cpuload")[0] as Double
+                            // ram
+                            val ramTotal = systemObject.getLong("mem_total")
+                            val ramFree = systemObject.getLong("mem_free")
+                            // swap
+                            val swapTotal = systemObject.getLong("swap_total")
+                            val swapFree = systemObject.getLong("swap_free")
+                            // disk
+                            val diskFree = systemObject.getDouble("freespace")
+
+                            updateViews(cpuLoad, ramTotal-ramFree, ramTotal,
+                                swapTotal-swapFree, swapTotal, diskFree)
 
 
-                        // TODO: sistemare questa percentuale
-                        // https://medium.com/devops-process-and-tools/how-to-measure-cpu-load-v-cpu-percentage-and-examples-da6057e39111
-                        cpu_load_placeholder.text = (((cpuLoadArray[0] as Double)*N_CORES)/100).toString()
+                        } else {
+                            cpu_layout.visibility = View.GONE
+                            ram_layout.visibility = View.GONE
+                            swap_layout.visibility = View.GONE
+                            disk_layout.visibility = View.GONE
+                            status_code_response_used_placeholder.text = statusCode.toString()
+                            status_message_response_placeholder.text = metaObject.getString("message")
+                        }
+
+
+
+
 
 
                         //val docs = json.getJSONArray("docs")
@@ -122,12 +151,31 @@ class MainActivity : WearableActivity() {
                     ram_layout.visibility = View.GONE
                     swap_layout.visibility = View.GONE
                     disk_layout.visibility = View.GONE
-                    status_code_response_used_placeholder.text = "404"
-                    status_message_response_placeholder.text = "Server not found"
+                    status_code_response_layout.visibility = View.GONE
+
+                    // showing the error message
+                    status_message_response_placeholder.text = e?.message
                 }
             }
         })
 
+    }
 
+    fun Double.format(digits: Int) = "%.${digits}f".format(this)
+
+    private fun updateViews(cpuLoad :Double, ramBusy :Long, ramTotal :Long, swapBusy :Long, swapTotal :Long, diskFree :Double)
+    {
+
+        val cpuLoad3Digit = Math.round(((cpuLoad*100)/N_CORES) * 1000.0) / 1000.0
+        val cpuLoad2Digit = Math.round(cpuLoad3Digit * 100.0) / 100.0
+
+        cpu_load_placeholder.text =  cpuLoad2Digit.toString()
+        ram_used_placeholder.text = (ramBusy / Byte).toString()
+        ram_total_placeholder.text = (ramTotal / Byte).toString()
+        swap_used_placeholder.text = (swapBusy /Byte).toString()
+        swap_total_placeholder.text = (swapTotal / Byte).toString()
+
+
+        disk_used_placeholder.text = (diskFree / GB).format(2)
     }
 }
