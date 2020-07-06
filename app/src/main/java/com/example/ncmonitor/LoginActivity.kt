07b1b52219c523d7479b597d5ef27aa238fea5dc
DavideAG <davideantonino94@gmail.com>
2020-07-06 12:32:05 +0200

    http - https radio buttons and dotted password

--- app/src/main/java/com/example/ncmonitor/LoginActivity.kt
+++ app/src/main/java/com/example/ncmonitor/LoginActivity.kt
@@ -6,9 +6,13 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import android.text.InputType
+import android.text.method.PasswordTransformationMethod
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.webkit.URLUtil
+import android.widget.RadioButton
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.Credentials.basic
@@ -39,6 +43,9 @@ class LoginActivity : WearableActivity()
 
         val sharedPref = getSharedPreferences(PREF_NAME, MODE_PRIVATE)
         val userInstance = sharedPref.getString(PREF_KEY, null)
+
+        // hiding the password using dotts
+        nc_password_input.transformationMethod = PasswordTransformationMethod.getInstance()
 
         if (userInstance != null) {
             //There is an instance of a user on the watch, retrieve data
@@ -73,8 +80,18 @@ class LoginActivity : WearableActivity()
 
     private fun showProgressBar()
     {
-        login_scrollview_layout.visibility = View.GONE
-        progressBar.visibility = View.VISIBLE
+        runOnUiThread {
+            login_scrollview_layout.visibility = View.GONE
+            progressBar.visibility = View.VISIBLE
+        }
+    }
+
+    private fun showLoginFields()
+    {
+        runOnUiThread {
+            login_scrollview_layout.visibility = View.VISIBLE
+            progressBar.visibility = View.GONE
+        }
     }
 
     private fun navigateToResults(responseData: String)
@@ -86,31 +103,40 @@ class LoginActivity : WearableActivity()
 
     private fun requestNcStatus(serverURL :String, username :String, password :String)
     {
-        Log.d("inputs","ServerURL: $serverURL \nnc_username: $username\nnc_password: $password")
         showProgressBar()
         val credentials = basic(username, password)
 
         request.GET(credentials, serverURL, object: Callback {
             override fun onResponse(call: Call?, response: Response) {
-                val userInfo = JSONObject()
-                userInfo.put("serverURL", serverURL)
-                userInfo.put("username", username)
-                userInfo.put("password", password)
-
-                val sharedPref = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
-
-                with(sharedPref.edit()) {
-                    putString(PREF_KEY, userInfo.toString())
-                    apply()
-                }
 
                 val responseData = response.body()?.string()
-
                 try {
-                    // navigate to the next activity
-                    navigateToResults(responseData!!)
-                    // now destroying the current activity
-                    finish()
+                    val json = JSONObject(responseData!!)
+                    val responseObject = json.getJSONObject("ocs")
+                    val metaObject = responseObject.getJSONObject("meta")
+                    val statusCode = metaObject.getInt("statuscode")
+
+                    if (statusCode == 200) {
+                        val userInfo = JSONObject()
+                        userInfo.put("serverURL", serverURL)
+                        userInfo.put("username", username)
+                        userInfo.put("password", password)
+
+                        val sharedPref = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+                        with(sharedPref.edit()) {
+                            putString(PREF_KEY, userInfo.toString())
+                            apply()
+                        }
+
+                        // navigate to the next activity
+                        navigateToResults(responseData)
+                        // now destroying the current activity
+                        finish()
+                    } else {
+                        showLoginFields()
+                        // navigate to the next activity
+                        navigateToResults(responseData)
+                    }
                 } catch (e: JSONException) {
                     Log.d("requestNcStatus","JsonException!!")
                     e.printStackTrace()
@@ -119,8 +145,7 @@ class LoginActivity : WearableActivity()
 
             override fun onFailure(call: Call?, e: IOException?) {
                 //todo: handle it showing info to the user
-                login_scrollview_layout.visibility = View.VISIBLE
-                progressBar.visibility = View.GONE
+                showLoginFields()
             }
         })
 
@@ -132,7 +157,9 @@ class LoginActivity : WearableActivity()
             nc_username_input.text.isNotEmpty() &&
             nc_password_input.text.isNotEmpty()) {
 
-            var serverURL = nc_server_input.text.toString()
+            var serverURL = attachProtocol()
+            serverURL += nc_server_input.text.toString()
+
             if (!URLUtil.isValidUrl(serverURL)) { //invalid url
                 Log.d("inputs","ServerURL: $serverURL \ninvalid url")
                 return
@@ -149,4 +176,12 @@ class LoginActivity : WearableActivity()
         }
     }
 
+    private fun attachProtocol(): String
+    {
+        return if (radio_button_http.isChecked) {
+            "http://"
+        } else {
+            "https://"
+        }
+    }
 }
